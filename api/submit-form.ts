9104d8c42b98cf25a8b3028f0ae1323f4edd18fa
Jason Etcovitch <jasonetco@github.com>
2020-06-07 18:46:49 -0400

    Use readme-box

--- api/submit-form.ts
+++ api/submit-form.ts
@@ -1,12 +1,7 @@
-import { Octokit } from '@octokit/rest'
 import { format } from 'date-fns'
 import { NowRequest, NowResponse } from '@vercel/node'
 import sanitizeHTML from 'sanitize-html'
-
-const REPO_DETAILS = {
-  owner: process.env.REPO_OWNER,
-  repo: process.env.REPO_OWNER
-}
+import { ReadmeBox } from 'readme-box'
 
 const MAX_ENTRIES = 5
 const MAX_LENGTH = 150
@@ -17,9 +12,6 @@ interface Guest {
   date: string
 }
 
-const START_COMMENT = '<!--START_SECTION:guestbook-->'
-const END_COMMENT = '<!--END_SECTION:guestbook-->'
-const listReg = new RegExp(`${START_COMMENT}[\\s\\S]+${END_COMMENT}`)
 const jsonReg = new RegExp(`<!--GUESTBOOK_LIST\\s(?<content>[\\s\\S]+)-->`)
 const entryTemplate = (guest: Guest) => {
   return `[@${guest.name}](https://github.com/@${guest.name}) says:
@@ -28,25 +20,6 @@ const entryTemplate = (guest: Guest) => {
 
 <sup>${guest.date}</sup>
 `
-}
-
-async function getReadme (octokit: Octokit) {
-  const res = await octokit.repos.getReadme(REPO_DETAILS)
-  const encoded = res.data.content
-  const decoded = Buffer.from(encoded, 'base64').toString('utf8')
-  return {
-    content: decoded,
-    sha: res.data.sha
-  }
-}
-
-function generateNewReadme (guests: Guest[], readme: string) {
-  const renderedList = renderList(guests)
-  const listWithFences = `${START_COMMENT}\n${renderedList}\n${END_COMMENT}`
-  const newContent = readme
-    .replace(listReg, listWithFences)
-    .replace(jsonReg, `<!--GUESTBOOK_LIST ${JSON.stringify(guests)}-->`)
-  return Buffer.from(newContent).toString('base64')
 }
 
 function parseListFromReadme (match: RegExpMatchArray): Guest[] {
@@ -67,8 +40,13 @@ function renderList (guests: Guest[]): string {
 }
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const octokit = new Octokit({ auth: `token ${process.env.GITHUB_TOKEN}` })
-  const readme = await getReadme(octokit)
+  const box = new ReadmeBox({
+    owner: process.env.REPO_OWNER,
+    repo: process.env.REPO_OWNER,
+    token: process.env.GITHUB_TOKEN
+  })
+
+  const readme = await box.getReadme()
 
   const match = readme.content.match(jsonReg)
   if (!match) return res.send(200).end()
@@ -82,17 +60,22 @@ export default async (req: NowRequest, res: NowResponse) => {
   }
 
   const newList = createNewList(newGuest, guests)
+  const renderedList = renderList(newList)
 
   try {
-    const newContents = generateNewReadme(newList, readme.content)
-  
-    await octokit.repos.createOrUpdateFile({
-      ...REPO_DETAILS,
-      content: newContents,
-      path: 'README.md',
-      message: `${newGuest.name} has signed the guestbook!`,
+    const newContents = box.replaceSection({
+      newContents: renderedList,
+      oldContents: readme.content,
+      section: 'guestbook'
+    })
+
+    const withJSONStore = newContents
+      .replace(jsonReg, `<!--GUESTBOOK_LIST ${JSON.stringify(guests)}-->`)
+
+    await box.updateReadme({
+      content: withJSONStore,
       sha: readme.sha,
-      branch: 'master'
+      path: readme.path
     })
 
     res.json({ redirect: 'https://github.com/JasonEtco' })
